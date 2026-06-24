b01ba09e088c3b616e07f77552e642c42ed10b8c
Alok Rathour <alokrathour261001@gmail.com>
2026-06-25 05:02:22 +0530

    program that will take three digits from the user and add the square of each digit.

---
+++ 18_sum_of_square_of_numbers.py
@@ -0,0 +1,7 @@
+#Write a program that will take three digits from the user and add the square of each digit.
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+num3 = int(input("Enter third number: "))
+
+sum_of_squares = (num1**2) + (num2**2) + (num3**2)
+print(sum_of_squares)
