8c9bc52176ad23d92d6d4b67733bac4538555b4c
Alok Rathour <alokrathour261001@gmail.com>
2026-06-25 02:16:37 +0530

    Swapping Two Numbers Using Third Variable

--- Find_Oldest_One.py
+++ Find_Oldest_One.py
@@ -1,5 +1,5 @@
 def find_oldest_age():
-    people = list(map(int, input().split()))
+    people = list(map(int, input("Enter 3 ages: ").split()))
     return max(people)
 
 oldest = find_oldest_age()
---
+++ Swap_Numbers.py
@@ -0,0 +1,8 @@
+#Using Third Variable
+num1 = int(input("Enter First Number :"))
+num2 = int(input("Enter Second Number :"))
+print("Numbers Before Swap : ", num1, num2)
+temp = num1
+num1 = num2
+num2 = temp
+print("Numbers After Swap : ", num1, num2)
