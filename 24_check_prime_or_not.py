a4dd812ada2a581d1e3bdfe81ae01321b66096dd
Alok Rathour <alokrathour261001@gmail.com>
2026-06-27 03:59:33 +0530

    whether a given number is prime number or not

---
+++ 24_check_prime_or_not.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a Number: "))
+count = 0
+
+for i in range(1,n+1):
+    if n%i == 0:
+        count += 1
+
+if count == 2:
+    print("Given Number is Prime.")
+else:
+    print ("Given Number is not prime.")
