5c68ccc12f1710bcfc3c65b92b69d79f22b61768
Alok Rathour <alokrathour261001@gmail.com>
2026-06-28 03:44:31 +0530

    Python menu program for cm-ft, km-miles, and USD-INR conversion.

--- 25_Population_Calculation.py
+++ 25_Population_Calculation.py
@@ -1,4 +1,12 @@
-population = 10000
+# The current population of a town is 10000. The population of the town is increasing at the rate of 10% per year. 
+# You have to write a program to find out the population at the end of each of the last 10 years. 
+# For eg current population is 10000 so the output should be like this:
+# 10th year - 10000
+# 9th year - 9000
+# 8th year - 8100 and so on
+
+
+population = int(input("enter the current population: "))
 years = 10
 for year in range(years, 0, -1):
     print(f"{year}th year - {int(population)}")
---
+++ 26_Menu_Driven_Program.py
@@ -0,0 +1,31 @@
+while True:
+    print("\n-----MENU-----")
+    print("1. cm to ft ")
+    print("2. kl to miles ")
+    print("3. usd to inr ")
+    print("4. exit ")
+
+    choice = int(input("Enter Your Choice: "))
+
+    if choice == 1:
+        cm  = float(input("Enter Length(cm): "))
+        ft = cm/30.48
+        print("Length in feet : ", ft)
+    
+    elif choice == 2:
+        km = float(input("Enter Distance(Km): "))
+        miles = km * 0.6214
+        print("Distance in Miles: ", miles)
+    
+    elif choice == 3:
+        usd = float(input("Enter Amount(usd): "))
+        inr = usd * 94
+        print("Amount in Indian Rupees: ", inr)
+    
+    elif choice == 4:
+        print("Exit")
+        break
+    
+    else:
+        print("Invalid Choice")
+    
